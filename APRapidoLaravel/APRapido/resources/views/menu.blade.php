41d1fda0ef5d15ec4aa24a98da5334b794fcfb90
Luiz Soares <luizotavioasoares@gmail.com>
2018-06-19 09:26:28 -0300

    inicio do desenvolvimento do menu

---
+++ APRapidoLaravel/APRapido/resources/views/menu.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.app');
+
+@section('content');
+
+    <div class="row">
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Criar APR
+            </div>
+        </div>
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Cadastro Ferramentas
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Visualizar APRs
+            </div>
+        </div>
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Cadastro Riscos
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Editar APR
+            </div>
+        </div>
+        <div class="col-lg-6" align="center">
+            <div class="btn btn-apr">
+                Cadastro MP
+            </div>
+        </div>
+    </div>
+    <div class="row">
+            <div class="col-lg-6" align="center">
+                <div class="btn btn-apr">
+                    Registro de Impressão
+                </div>
+            </div>
+            <div class="col-lg-6" align="center">
+                <div class="btn btn-apr">
+                    Cadastro Responsável
+                </div>
+            </div>
+        </div>
+@endsection;
