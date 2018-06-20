82a9485ff22d72a73e681500c4ce0fda756711ed
Luiz Soares <luizotavioasoares@gmail.com>
2018-06-20 17:59:42 -0300

    layout e menu

--- APRapidoLaravel/APRapido/resources/views/menu.blade.php
+++ APRapidoLaravel/APRapido/resources/views/menu.blade.php
@@ -1,53 +1,53 @@
-@extends('layout.app');
+@extends('layout.app')
 
-@section('content');
+@section('content')
 
-    <div class="row">
-        <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+    <div class="row mb-2 mt-5">
+        <div class="col-lg-6 mb-2" align="center">
+            <div class="btn btn-primary">
                 Criar APR
             </div>
         </div>
         <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+            <div class="btn btn-primary">
                 Cadastro Ferramentas
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+        <div class="col-lg-6 mb-2 mt-2" align="center">
+            <div class="btn btn-primary">
                 Visualizar APRs
             </div>
         </div>
-        <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+        <div class="col-lg-6 mb-2 mt-2" align="center">
+            <div class="btn btn-primary">
                 Cadastro Riscos
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+        <div class="col-lg-6 mb-2 mt-2" align="center">
+            <div class="btn btn-primary">
                 Editar APR
             </div>
         </div>
-        <div class="col-lg-6" align="center">
-            <div class="btn btn-apr">
+        <div class="col-lg-6 mb-2 mt-2" align="center">
+            <div class="btn btn-primary">
                 Cadastro MP
             </div>
         </div>
     </div>
     <div class="row">
-            <div class="col-lg-6" align="center">
-                <div class="btn btn-apr">
+            <div class="col-lg-6 mb-2 mt-2" align="center">
+                <div class="btn btn-primary">
                     Registro de Impressão
                 </div>
             </div>
-            <div class="col-lg-6" align="center">
-                <div class="btn btn-apr">
+            <div class="col-lg-6 mb-2 mt-2" align="center">
+                <div class="btn btn-primary">
                     Cadastro Responsável
                 </div>
             </div>
         </div>
-@endsection;
+@endsection
