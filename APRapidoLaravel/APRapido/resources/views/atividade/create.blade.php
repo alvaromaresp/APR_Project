fea3f598ee1c38faad7e96b66924f9c371670905
Luiz Soares <luizotavioasoares@gmail.com>
2018-06-25 14:48:28 -0300

    alterações atividade

--- APRapidoLaravel/APRapido/resources/views/atividade/create.blade.php
+++ APRapidoLaravel/APRapido/resources/views/atividade/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    {!! Form::open(['action' => 'atividadeController@store', 'method' => 'post']) !!}
+    {!! Form::open(['action' => 'AtividadeController@store', 'method' => 'post']) !!}
         <div class="form-group mt-5 ml-5 mr-5 mb-5">
             <h2> {{Form::label('atividade', 'Nova Atividade')}} </h2>
             {{Form::text('atividade', '', ['class' => 'form-control mt-3 mb-3', 'placeholder' => 'Atividade'])}}
