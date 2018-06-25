e077bf6f56464fd7b9ce29675cc87c88aeb2c18d
Luiz Soares <luizotavioasoares@gmail.com>
2018-06-25 14:13:12 -0300

    inicio crud sesmt

--- APRapidoLaravel/APRapido/resources/views/sesmt/create.blade.php
+++ APRapidoLaravel/APRapido/resources/views/sesmt/create.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 
-    {!! Form::open(['action' => 'empresaController@store', 'method' => 'post']) !!}
+    {!! Form::open(['action' => 'SesmtController@store', 'method' => 'post']) !!}
         <div class="form-group mt-5 ml-5 mr-5 mb-5">
-            <h2> {{Form::label('empresa', 'Nova Empresa')}} </h2>
-            {{Form::text('empresa', '', ['class' => 'form-control mt-3 mb-3', 'placeholder' => 'Empresa'])}}
+            <h2> {{Form::label('nome', 'Nova Sesmt')}} </h2>
+            {{Form::text('nome', '', ['class' => 'form-control mt-3 mb-3', 'placeholder' => 'Nome'])}}
+            {{Form::text('telefone', '', ['class' => 'form-control mt-3 mb-3', 'placeholder' => 'Telefone'])}}
    			{{Form::submit('Enviar', ['class' => 'btn btn-success mt-3 float-right'])}}
    		</div>
     {!! Form::close() !!}
--- APRapidoLaravel/APRapido/resources/views/sesmt/index.blade.php
+++ APRapidoLaravel/APRapido/resources/views/sesmt/index.blade.php
@@ -5,22 +5,20 @@
     @include('inc.messages')
 
 
-    @if(count($empresa) > 0)
     <div class="mt-5 ml-5 mr-5 mb-5">
     	<h1>Empresa
-    	<a href="/empresa/create" class="btn btn-secondary float-right mt-2 mb-3">Criar Nova</a></h1>
+    	<a href="/sesmt/create" class="btn btn-secondary float-right mt-2 mb-3">Criar Nova</a></h1>
     	<div class="input-group mb-4">
 		  <input type="text" class="form-control">
 		  <div class="input-group-append">
 		    <button class="btn btn-secondary" type="button">Buscar</button>
 		  </div>
 		</div>
-        @foreach($empresa as $emp)
-            <h3><a href="/empresa/{{$emp->id}}"> {{$emp->empresa}} </a></h3> <br>
+        @foreach($sesmt as $s)
+            <h3><a href="/sesmt/{{$s->id}}"> {{$s->nome}} </a></h3> <br>
         @endforeach
     </div>
 
-    <div class="float-right">{{$empresa->link()}}</div>
-    @endif
+    <div class="float-right">{{$sesmt->links()}}</div>
 
 @endsection
