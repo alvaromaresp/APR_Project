5114c5416a3060497773ef6cd8f9ed5b51970b58
Aline Guimarães <alinerguio@gmail.com>
2018-07-18 11:09:39 -0300

    mudanças de css

--- APRapidoLaravel/APRapido/resources/views/layout/app.blade.php
+++ APRapidoLaravel/APRapido/resources/views/layout/app.blade.php
@@ -1,16 +1,14 @@
 <DOCTYPE html>
-    <html>
+    <html lang="{{app()->getLocale() }}">
         <head>
-
+            <title>{{config('app.name','APRápido')}}</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-            <script
-                    src="https://code.jquery.com/jquery-3.3.1.min.js"
-                    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-                    crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
             <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
             <style>
                 @media (min-width: 992px) {
                     .bigButton {
@@ -113,7 +111,9 @@
                     color: #e6e6e6;
                 }
             </style>
+
         </head>
+
         <body>
             <div class="container mb-5 mt-5" style="border: 1vh solid #00004c">
                 <div class="row mt-4 mb-4">
@@ -125,7 +125,7 @@
                         </div>
                         <div class="row">
                             <div class="col offset-lg-3 caminho">
-                                <p>> Menu</p>
+                                <p> > Menu</p>
                             </div>
                         </div>
                     </div>
