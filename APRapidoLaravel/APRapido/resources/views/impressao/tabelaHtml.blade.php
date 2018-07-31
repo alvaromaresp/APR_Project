f808aa5ce6075d0fb09f5147193d236408099476
Aline Guimarães <alinerguio@gmail.com>
2018-07-31 19:49:16 -0300

    pdf

--- APRapidoLaravel/APRapido/resources/views/impressao/tabelaHtml.blade.php
+++ APRapidoLaravel/APRapido/resources/views/impressao/tabelaHtml.blade.php
@@ -1,5 +1,214 @@
 <link rel="stylesheet" href="{{ public_path('css/grid_bootstrap.css') }}">
 
+<div class="container-fluid borda">
+    <!-- CABEÇALHO --><br>
+    <div class="row bordaBaixa">
+        <div class="col-4">
+            <div class="row">
+                <div class="col">
+                    <img  class="cem mt-2 float-right" src="{{public_path('img/farmax.png')}}"/>
+                </div>
+                <div class="col">
+                    <img  class="cem mt-2 float-left" src="{{public_path('img/icot.png')}}"/>
+                </div>
+            </div>
+        </div>
+        <div class="col txt-centro h4 mt-2">
+            <b>ANÁLISE PRELIMINAR DE RISCOS</b>
+        </div>
+        <div class="col-4">
+            <div class="row txt-dir">
+                <div class="col">
+                    APR N°: id apr
+                </div>
+                <div class="col">
+                    Data: data do dia
+                </div>
+                <div class="col">
+                    Pág: paginacao
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+    </div>
+
+    <!-- NATUREZA DOS RISCOS -->
+    <div class="row">
+        <div class="col-4 txt-centro bordaBaixa bordaLado">
+            NATUREZA DOS RISCOS<br/>
+            Caso marcar um item 'S' deve abrir uma PT.
+        </div>
+        <div class="col">
+            <div class="row bordaBaixa">
+                <div class="col">
+                    <div class="quadradin mt-1">
+                        S
+                    </div>
+                    <div class="quadradin float-left">
+                        NA
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="quadradin mt-1">
+                        NA
+                    </div>
+                    aline
+                    <div class="quadradin">
+                        S
+                    </div>
+                </div>
+                <div class="col">
+                    3
+                </div>
+                <div class="col">
+                    <br><br>
+                    4
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SETOR E LOCAL -->
+    <div class="row bordaBaixa">
+        <div class="col-4 bordaLado">
+            <b>SETOR: </b>local 
+        </div>
+        <div class="col">
+            <b>LOCAL DO SERVIÇO: </b> celula
+        </div>
+    </div>
+
+    <!-- TAREFA -->
+    <div class="row bordaBaixa">
+        <div class="col">
+            <b>TAREFA:</b>nome apr
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col txt-centro bordaBaixa">
+            <b>Cabe ao contratado a responsabilidade de treinar todos os executantes nas tarefas definidas nesse documento, evidenciando com as assinaturas no verso.</b>
+        </div>
+    </div>
+    <!-- CORDENAÇÃO DE SERVIÇO SESMT RESPONSÁVEL PELA ÁREA -->
+    <div class="row bordaBaixa">
+        <div class="col bordaLado txt-centro">
+           <b> CORDENAÇÃO DE SERVIÇO </b>
+        </div>
+        <div class="col bordaLado txt-centro">
+           <b> SESMT </b>
+        </div>
+        <div class="col txt-centro">
+            <b> RESPONSÁVEL PELA ÁREA </b>
+        </div>
+    </div>
+    <div class="row bordaBaixa">
+        <div class="col bordaLado">
+            Nome: coordenador<br/>
+            Tel: +55 (37) 0000-0000<br/><br/>
+            Assinatura:
+        </div>
+        <div class="col bordaLado">
+            Nome: Rafaela Sousa<br/>
+            Tel: +55 (37) 2101-9651<br/><br/>
+            Assinatura:
+        </div>
+        <div class="col">
+            Nome:<br/>
+            Tel: +55 (37) 0000-0000<br/><br/>
+            Assinatura:
+        </div>
+    </div>
+
+
+    <!-- ATIVIDADES RISCOS POTENCIAIS MEDIDAS PREVENTIVAS/RECOMENDAÇÕES DE SEGURANÇA -->
+    <div class="row txt-centro bordaBaixa">
+        <div class="col bordaLado">
+            <b>ATIVIDADES</b>
+        </div>
+        <div class="col bordaLado">
+            <b>RISCOS POTENCIAIS</b>
+        </div>
+        <div class="col">
+            <b>MEDIDAS PREVENTIVAS/RECOMENDAÇÕES DE
+            SEGURANÇA</b>
+        </div>
+    </div>
+    <div class="row txt-centro bordaBaixa">
+        <div class="col bordaLado">
+            (com suas respectivas etapas/passos)
+        </div>
+        <div class="col bordaLado">
+            (o que poderá sair de errado)
+        </div>
+        <div class="col">
+            (evitar o acidente ou minimizar os danos, caso ocorra)
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        @foreach($data['atividade'] as $atv)
+            <div class="row bordaBaixa">
+                <div class="col bordaLado">{{$atv->atividade}}</div>
+
+                <?php
+                $bf = true;
+                ?>
+
+                @foreach($atv->Ferramentas as $fer)
+                    @if(!$bf)
+                        <div class="col bordaLado">EMPTY</div>
+                    @endif
+
+                    <div class="col">{{$fer->ferramenta}}</div>
+
+                    <?php
+                    $br = true;
+                    ?>
+
+                    @foreach($fer->Riscos as $ris)
+                        @if(!$br)
+                            <div class="col">EMPTY</div>
+                            <div class="col">EMPTY</div>
+                        @endif
+                        <div class="col">{{$ris->risco}}</div>
+
+                        <?php
+                        $bmp = true;
+                        ?>
+                        @foreach($ris->medidaspreventivas as $mp)
+                            @if(!$bmp)
+                                <div class="col">EMPTY</div>
+                                <div class="col">EMPTY</div>
+                                <div class="col">EMPTY</div>
+                            @endif
+                            <div class="col">{{$mp->medidapreventiva}}</div>
+
+            </div>
+            <?php
+            $bmp = false;
+            ?>
+        @endforeach
+
+        <?php
+        $br = false;
+        ?>
+        @endforeach
+
+        <?php
+        $bf = false;
+        ?>
+        @endforeach
+
+        @endforeach
+    </div>
+
+
+</div>
+<br><br>
+
 <table border="2px" bordercolor="black">
 <tr>
   <td colspan='5'>
@@ -193,200 +402,4 @@
 </table>
 
 <br><br>
-
-
-<div class="container-fluid">
-    <!-- CABEÇALHO -->
-    <div class="row">
-        <div class="col-4">
-            <div class="row">
-                <div class="col">
-                    <img  class="cem" src="{{public_path('img/farmax.png')}}"/>
-                </div>
-                <div class="col">
-                    <img  class="cem" src="{{public_path('img/icot.png')}}"/>
-                </div>
-            </div>
-        </div>
-        <div class="col txt-centro h5">
-            ANÁLISE PRELIMINAR DE RISCOS
-        </div>
-        <div class="col-4">
-            <div class="row txt-dir">
-                <div class="col">
-                    APR N°: 0019
-                </div>
-                <div class="col">
-                    Data: 21/07/2018
-                </div>
-                <div class="col">
-                    Pág: 1/3
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- NATUREZA DOS RISCOS -->
-    <div class="row">
-        <div class="col-4 txt-centro">
-            NATUREZA DOS RISCOS<br/>
-            Caso marcar um item 'S' deve abrir uma PT.
-        </div>
-        <div class="col">
-            <div class="row">
-                <div class="col">
-                    1
-                </div>
-                <div class="col">
-                    2
-                </div>
-                <div class="col">
-                    3
-                </div>
-                <div class="col">
-                    4
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- SETOR E LOCAL -->
-    <div class="row">
-        <div class="col-4">
-            SETOR: ALMOXARIFADO COSMETICO II
-        </div>
-        <div class="col">
-            LOCAL DO SERVIÇO:
-        </div>
-    </div>
-
-    <!-- TAREFA -->
-    <div class="row">
-        <div class="col">
-            Tarefa: khdasah
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            Cabe ao contratado a responsabilidade de treinar todos os executantes nas tarefas definidas nesse documento, evidenciando com as assinaturas no verso.
-        </div>
-    </div>
-    <!-- CORDENAÇÃO DE SERVIÇO SESMT RESPONSÁVEL PELA ÁREA -->
-    <div class="row">
-        <div class="col">
-            CORDENAÇÃO DE SERVIÇO
-        </div>
-        <div class="col">
-            SESMT
-        </div>
-        <div class="col">
-            RESPONSÁVEL PELA ÁREA
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            Nome: coordenador<br/>
-            Tel: +55 (37) 0000-0000<br/><br/>
-            Assinatura:
-        </div>
-        <div class="col">
-            Nome: Rafaela Sousa<br/>
-            Tel: +55 (37) 2101-9651<br/><br/>
-            Assinatura:
-        </div>
-        <div class="col">
-            Nome:<br/>
-            Tel: +55 (37) 0000-0000<br/><br/>
-            Assinatura:
-        </div>
-    </div>
-
-
-    <!-- ATIVIDADES RISCOS POTENCIAIS MEDIDAS PREVENTIVAS/RECOMENDAÇÕES DE SEGURANÇA -->
-    <div class="row">
-        <div class="col">
-            ATIVIDADES
-        </div>
-        <div class="col">
-            RISCOS POTENCIAIS
-        </div>
-        <div class="col">
-            MEDIDAS PREVENTIVAS/RECOMENDAÇÕES DE
-            SEGURANÇA
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            (com suas respectivas etapas/passos)
-        </div>
-        <div class="col">
-            (o que poderá sair de errado)
-        </div>
-        <div class="col">
-            (evitar o acidente ou minimizar os danos, caso ocorra)
-        </div>
-    </div>
-
-
-
-    <div class="row">
-        @foreach($data['atividade'] as $atv)
-            <div class="row">
-                <div class="col">{{$atv->atividade}}</div>
-
-                <?php
-                $bf = true;
-                ?>
-
-                @foreach($atv->Ferramentas as $fer)
-                    @if(!$bf)
-                        <div class="col">EMPTY</div>
-                    @endif
-
-                    <div class="col">{{$fer->ferramenta}}</div>
-
-                    <?php
-                    $br = true;
-                    ?>
-
-                    @foreach($fer->Riscos as $ris)
-                        @if(!$br)
-                            <div class="col">EMPTY</div>
-                            <div class="col">EMPTY</div>
-                        @endif
-                        <div class="col">{{$ris->risco}}</div>
-
-                        <?php
-                        $bmp = true;
-                        ?>
-                        @foreach($ris->medidaspreventivas as $mp)
-                            @if(!$bmp)
-                                <div class="col">EMPTY</div>
-                                <div class="col">EMPTY</div>
-                                <div class="col">EMPTY</div>
-                            @endif
-                            <div class="col">{{$mp->medidapreventiva}}</div>
-
-            </div>
-            <?php
-            $bmp = false;
-            ?>
-        @endforeach
-
-        <?php
-        $br = false;
-        ?>
-        @endforeach
-
-        <?php
-        $bf = false;
-        ?>
-        @endforeach
-
-        @endforeach
-    </div>
-
-
-</div>
 
