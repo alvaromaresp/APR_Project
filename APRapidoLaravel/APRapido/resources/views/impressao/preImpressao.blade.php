eb4213a1d025a873f33e4abcce7ad9607f4ac918
Aline Guimarães <alinerguio@gmail.com>
2018-08-14 13:37:53 -0300

    impressao

--- APRapidoLaravel/APRapido/resources/views/impressao/preImpressao.blade.php
+++ APRapidoLaravel/APRapido/resources/views/impressao/preImpressao.blade.php
@@ -28,9 +28,9 @@
         {{Form::label('empresa', 'Selecione a Empresa:')}}
         {{Form::select('empresa',$data['empresas'],null,['class' => 'form-control mt-2 mb-3'])}}
 
-        {{Form::submit('Enviar', ['class' => 'btn btn-success mt-3 float-right'])}}
+        {{Form::submit('Imprimir', ['class' => 'btn btn-success mt-3 float-right'])}}
         <div class="float-left">
-            <a href="/apr" class="btn mt-3 btn-secondary">Voltar</a>
+            <a href="/apr" class="btn mt-3 btn-secondary">Menu</a>
         </div>
     </div>
     {!! Form::close() !!}
@@ -59,9 +59,9 @@
         {{Form::label('empresa', 'Selecione a Empresa:')}}
         {{Form::select('empresa',$data['empresas'],null,['class' => 'form-control mt-2 mb-3'])}}
 
-        {{Form::submit('Enviar', ['class' => 'btn btn-success mt-3 float-right'])}}
+        {{Form::submit('Imprimir', ['class' => 'btn btn-success mt-3 float-right'])}}
         <div class="float-left">
-            <a href="/apr" class="btn mt-3 btn-secondary">Voltar</a>
+            <a href="/apr" class="btn mt-3 btn-secondary">Menu</a>
         </div>
     </div>
     {!! Form::close() !!}
